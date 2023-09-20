c7f02bed72fbf754e6dd1fad60239d8d0da2349d
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-20 09:37:40 -0300

    fix: Perfil component position

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,13 +1,10 @@
 import styled from 'styled-components';
 
 export const Main = styled.main`
-  /* background-color: red; */
   width: 100vw;
   height: 100dvh;
 
-
   .perfil {
-    position: absolute;
     z-index: 10;
     top: 2rem;
     left: 2rem;
