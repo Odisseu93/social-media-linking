0bc05830a99a2e7eb2b4cd94ea5860d4473524f6
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-20 17:47:08 -0300

    feat: import icons

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,12 @@
+import { BsGithub, BsLinkedin, BsDiscord } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md';
+import { PiSlackLogoThin } from 'react-icons/pi';
+
+
+export default {
+	gitHub: BsGithub,
+	linkedIn: BsLinkedin,
+	email: MdEmail,
+	discord: BsDiscord,
+	slack: PiSlackLogoThin,
+};
