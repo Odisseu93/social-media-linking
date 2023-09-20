28289641c8b4c90bc3c9c7bef2849fafa9470f52
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-19 22:00:26 -0300

    fix: createRoot bug error TS2339: Property 'createRoot' does not exist

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
 import { GlobalsStyles } from './styles/globals.ts';
-
-import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<GlobalsStyles />
 		<App />
-	</React.StrictMode>,
+	</React.StrictMode>
 );
