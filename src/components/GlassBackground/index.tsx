7a0c7a8f06a5cbf89bf4248496dbbbe9542280db
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-20 19:09:46 -0300

    refactor: remove unnecessary prop on GrassBackground component

--- src/components/GlassBackground/index.tsx
+++ src/components/GlassBackground/index.tsx
@@ -1,11 +1,6 @@
 import * as C from './styles';
 
-type GrassBackgroundProps = {
-  children: React.ReactNode;
-}
-
 /** backgroud with glass effect */
-const GrassBackground = ({children}: GrassBackgroundProps)=>(
-	<C.GrassBackground>{children}</C.GrassBackground>);
+const GrassBackground = ()=>(<C.GrassBackground/>);
 
 export default GrassBackground;
