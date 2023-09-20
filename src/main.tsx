0b9f2c6549247f8d16a5e60f1ae389e4e4876a65
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-20 17:44:24 -0300

    build: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.tsx';
 
 import { GlobalsStyles } from './styles/globals.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<GlobalsStyles />
-		<App />
+		<BrowserRouter>
+			<GlobalsStyles />
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
