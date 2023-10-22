0d6f5419f236bdd00c199cbb7295f60dad847999
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-10-21 21:14:08 -0300

    feat: add css animation

--- src/style.ts
+++ src/style.ts
@@ -1,9 +1,19 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
+const slidein = keyframes` 
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+`;
 
 export const Content = styled.div`
   max-height: 90vh;
   max-width: 1440px;
 
   padding-bottom: 10rem;
+  animation: 3s ${slidein} ease-out;
 `;
