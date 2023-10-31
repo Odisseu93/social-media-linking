48db30e3f5d63a57d7b8cb892acdc47203fc0c21
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-10-31 19:59:26 -0300

    feat: add CodePen link

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -2,6 +2,8 @@ import { BsGithub, BsLinkedin, BsDiscord , BsYoutube } from 'react-icons/bs';
 import { MdEmail } from 'react-icons/md';
 import { PiSlackLogoThin } from 'react-icons/pi';
 import { AiOutlineFilePdf } from 'react-icons/ai';
+import { AiFillCodepenSquare } from 'react-icons/ai';
+
 
 export default {
 	gitHub: BsGithub,
@@ -11,4 +13,5 @@ export default {
 	slack: PiSlackLogoThin,
 	youtube: BsYoutube,
 	pdf: AiOutlineFilePdf,
+	codepen: AiFillCodepenSquare
 };
