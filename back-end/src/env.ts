707e6f77c813ff10193c590c33eb1e37434a2bc8
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2025-04-08 20:32:04 -0300

    feat(back-end): add more environments variables

--- back-end/src/env.ts
+++ back-end/src/env.ts
@@ -3,6 +3,8 @@ import { loadEnv } from 'vkrun';
 loadEnv();
 
 export const env = {
-	APP_PORT : process.env.PORT || 8080,
-	ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS?.split(',') ?? undefined
+	APP_PORT : process.env.APP_PORT || 8080,
+	ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS?.split(',') ?? undefined,
+	TURSO_DATABASE_URL:	process.env.TURSO_DATABASE_URL as string,
+	TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN as string,
 };
