f2c496ae48c05738329566c3640cf8620873a10e
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2025-04-08 20:33:45 -0300

    feat(back-end): add `/links` endpoint

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,5 +1,6 @@
 import v from 'vkrun';
 import { env } from './env';
+import db from './database';
 
 const corsCofig: v.CorsSetOptions = {
 	origin: env.ALLOWED_DOMAINS
@@ -15,6 +16,23 @@ app.use(cors(corsCofig));
 
 app.get('/test', (req: v.Request, res: v.Response) => {
 	res.send('Hello my friend!');
+});
+
+
+app.get('/links', async (req: v.Request, res: v.Response) => {
+
+	const result = await db.execute({
+		sql: 'SELECT * FROM links',
+	});
+
+	const links = result.rows;
+
+	if(!links || links?.length === 0) {
+		res.status(404).json({ message: 'Not found links' });
+	}
+
+	res.status(200).json({ links });
+
 });
 
 
