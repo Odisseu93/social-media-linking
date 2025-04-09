8d03a3a447e486696f90e9ce3679894efc56d971
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2025-04-09 09:37:38 -0300

    style: change how the "env" variable is exported

--- back-end/src/env.ts
+++ back-end/src/env.ts
@@ -2,9 +2,11 @@ import { loadEnv } from 'vkrun';
 
 loadEnv();
 
-export const env = {
+const env = {
 	APP_PORT : process.env.APP_PORT || 8080,
 	ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS?.split(',') ?? undefined,
 	TURSO_DATABASE_URL:	process.env.TURSO_DATABASE_URL as string,
 	TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN as string,
 };
+
+export { env };
